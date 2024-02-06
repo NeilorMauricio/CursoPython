06230c68306340708a0cf3d33bbf700d51beac7e
Neilor Mauricio <neilormauricio@gmail.com>
2024-02-06 15:30:13 -0300

    Create ex004.py

---
+++ ex004.py
@@ -0,0 +1,17 @@
+a = str(input('Digite algo: '))
+print(f''' 
+    É Alpha númerico? {a.isalnum()},
+    É alpha? {a.isalpha()},
+    É Minuscula? {a.islower()},
+    É maiuscula? {a.isupper()},
+    Está capitalizada? {a.istitle()},
+    É Decimal? {a.isdecimal()},
+    É numérica? {a.isnumeric()},
+    É espaço? {a.isspace()},
+    Tipo: {type(a)}
+      ''')
+
+
+
+
+
