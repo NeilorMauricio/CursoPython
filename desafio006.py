c36d3923f1377fc9d134dd90995cd8f4a6ed3afb
Neilor Mauricio <neilormauricio@gmail.com>
2024-02-06 16:48:07 -0300

    Create desafio006.py
    # Crie um algoritimo que leia um número e mostre seu dobro, triplo e raiz quadrada

---
+++ desafio006.py
@@ -0,0 +1,12 @@
+# Crie um algoritimo que leia um número e mostre seu dobro, triplo e raiz quadrada
+
+n = int(input('Digite um número:'))
+print(f'\n'
+      f'O número digitado foi: {n} \n'
+      f'O dobro é: {n*2} \n'
+      f'O triplo é: {n*3} \n'
+      f'A raiz quadrada é: {n**(1/2)}')
+
+
+
+
