5f87f0c33e5672ee2bd5ef5eb00443247b33e731
Neilor Mauricio <neilormauricio@gmail.com>
2024-02-06 17:58:09 -0300

    Create ex011.py
    # Pintando parede

---
+++ ex011.py
@@ -0,0 +1,9 @@
+# Pintando parede
+
+largura = float(input('Digite a largura da parede '))
+altura = float(input('Digite a altura da parede '))
+area = largura * altura
+tinta = area / 2
+print(f'A área da parede é de {area:.2f}m²')
+print(f'Você vai precisar  de {tinta:.2f}L')
+
