6066786335ccf0c03c5c3f677f148fcf87b70cbd
Neilor Mauricio <neilormauricio@gmail.com>
2024-02-06 17:45:38 -0300

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,7 +1,7 @@
 
 
 # Conversor de moedas:
-dol = int(input('Digite o valor em dólar: '))
+dol = float(input('Digite o valor em dólar: '))
 brl = dol * 5
 
 print(f'{dol:.2f} dólares convertidos para real: {brl:.2f}')
