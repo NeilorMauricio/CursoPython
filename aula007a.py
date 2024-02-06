cf6e01505381bf24fe572f465bd174f8ea21e768
Neilor Mauricio <neilormauricio@gmail.com>
2024-02-06 16:34:32 -0300

    Create aula007a.py
    # Faça um programa que leia um número e mostre seu sucessor e antecessor.

---
+++ aula007a.py
@@ -0,0 +1,10 @@
+# Faça um programa que leia um número e mostre seu sucessor e antecessor.
+n = int(input('Digite um número:'))
+print(f'\n'
+      f'O número digitado foi: {n} \n'
+      f'O número inteiro é: {int(n)} \n'
+      f'O número real é: {float(n)} \n'
+      f'O número inteiro mais 1 é: {int(n) + 1} \n'
+      f'O número inteiro menos 1 é: {int(n) - 1} \n'
+      f'O número inteiro multiplicado por 2 é: {int(n) * 2} \n'
+      f'O número inteiro dividido por 2 é: {int(n) / 2} \n')
